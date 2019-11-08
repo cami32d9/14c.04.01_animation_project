541fea495db4395fbebb1b5c2158b7933a4ea777
Camilla Gejl Olsen <cami32d9@stud.kea.dk>
2019-11-08 11:36:35 +0100

    Fetching information from json

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", getJson);
+
+async function getJson() {
+    let pagesUrl = "https://mandalskeawebspace.dk/claude_php/clean_up_spreadsheet.php?id=1uLNO5Z7ghpMy283IND0YbnVHK-gW6VIGHrCk39T_DRo";
+    let jsonData = await fetch(pagesUrl);
+    const deathArray = await jsonData.json();
+    start(deathArray);
+}
+
+function start(deathArray) {
+    console.log(deathArray);
+    showTimeline(deathArray);
+}
+
+
+function showTimeline(deathArray) {
+
+    deathArray.forEach(fatality => {
+        const template = document.querySelector(".timeline_template").content.cloneNode(true);
+
+        template.querySelector(".timeline_name").textContent = fatality.name;
+
+        document.querySelector(".book").appendChild(template);
+
+    });
+}
