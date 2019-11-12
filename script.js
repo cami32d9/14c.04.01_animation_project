41fa404e6ab460cfad95c4ee20c55cd73194192a
Camilla Gejl Olsen <cami32d9@stud.kea.dk>
2019-11-12 13:06:45 +0100

    Added comments to JavaScript, and added is/is not touch-function to destinguish between the two, and made hover function on is-not-touch work with this.

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", getJson);
+document.addEventListener("DOMContentLoaded", getTimelineJson);
 
 let itemID = 0;
 let nameSVG;
 let svgIndex = 0;
 let fatalityIndex;
 
-async function getJson() {
+
+// ----- CHECKING IF TOUCH- OR MOUSE DEVICE -----
+
+const touchDeviceClass = getTouchDeviceClass();
+
+function getTouchDeviceClass() {
+    return isTouchDeviceFunction() ? "touch_device_item" : "not_touch_device_item"
+}
+
+// Function that checks if the device is a touch device or not.
+// bolmaster2, updated 2018.
+// https://stackoverflow.com/questions/4817029/whats-the-best-way-to-detect-a-touch-screen-device-using-javascript/19299994#19299994
+function isTouchDeviceFunction() {
+    var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
+    var mq = function (query) {
+        return window.matchMedia(query).matches;
+    };
+
+    if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+        return true;
+    }
+
+    // include the 'heartz' as a way to have a non matching MQ to help terminate the join
+    // https://git.io/vznFH
+    var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
+    return mq(query);
+}
+
+
+// ----- FETCHING DATA -----
+
+// Fetching the data for all deaths defined in a Google Spreadsheet.
+async function getTimelineJson() {
     let pagesUrl =
         "https://mandalskeawebspace.dk/claude_php/clean_up_spreadsheet.php?id=1uLNO5Z7ghpMy283IND0YbnVHK-gW6VIGHrCk39T_DRo";
     let jsonData = await fetch(pagesUrl);
@@ -15,6 +47,7 @@ async function getJson() {
     fetchSVGs(deathArray);
 }
 
+// Fetching the necessary SVGs (currently only the animated names) for each person found in the json fetched before.
 function fetchSVGs(deathArray) {
 
     deathArray.forEach(fatality => {
@@ -29,64 +62,64 @@ function fetchSVGs(deathArray) {
                     fatality.namesvg = nameSVG;
                     svgIndex++;
                     if (svgIndex === deathArray.length) {
-                        start(deathArray);
+                        showTimeline(deathArray, 0);
                     }
                 }
             );
     });
 }
 
-function start(deathArray) {
-    console.log(deathArray);
-    showTimeline(deathArray, 0);
-}
-
-let fatality;
-
+// Shows the timeline with the deathArray defined from the json, and the "fatalityIndex" - an index making sure the
+// function loops over each person in the array. This method is chosen over forEach to be able to delay each line's
+// appearance on the timeline with a setTimeout.
 function showTimeline(deathArray, getFatalityIndex) {
 
     fatalityIndex = getFatalityIndex;
 
     const fatality = deathArray[fatalityIndex];
 
-        const timelineTemplate = document
-            .querySelector(".timeline_template")
-            .content.cloneNode(true);
-        fatality.name = `${fatality.firstname} ${fatality.lastname}`;
-        itemID++;
+    const timelineTemplate = document
+        .querySelector(".timeline_template")
+        .content.cloneNode(true);
 
-        // template.querySelector(".timeline_icon").textContent = fatality.name;
-        timelineTemplate
-            .querySelector(".fatality_item")
-            .setAttribute("timelineItemID", `${itemID}`);
-        timelineTemplate
-            .querySelector(".infobox")
-            .setAttribute("itemID", `${itemID}`);
 
-        timelineTemplate.querySelector(".timeline_icon").src = `elements/${fatality.deathcategory}.svg`;
-        timelineTemplate.querySelector(".timeline_name").innerHTML = fatality.namesvg;
-        timelineTemplate.querySelector(".timeline_death_date").textContent =
-            fatality.death;
+    // Adding details to the item on the timeline.
+    timelineTemplate
+        .querySelector(".fatality_item")
+        .setAttribute("timelineItemID", `${itemID}`);
+    timelineTemplate
+        .querySelector(".infobox")
+        .setAttribute("itemID", `${itemID}`);
 
-        timelineTemplate.querySelector(
-            ".infobox_image"
-        ).src = `elements/${fatality.firstname}.svg`;
-        timelineTemplate.querySelector(
-            ".infobox_name"
-        ).innerHTML = fatality.namesvg;
-        timelineTemplate.querySelector(".infobox_birth_date").innerHTML +=
-            fatality.birth || "Unknown";
-        timelineTemplate.querySelector(".infobox_death_date").innerHTML +=
-            fatality.death;
-        timelineTemplate.querySelector(".infobox_story").innerHTML +=
-            fatality.story;
-        timelineTemplate.querySelector(".infobox_death_cause").innerHTML +=
-            fatality.causeofdeath;
+    timelineTemplate.querySelector(".fatality_item").classList.add(touchDeviceClass);
+    timelineTemplate.querySelector(".timeline_icon").src = `elements/${fatality.deathcategory}.svg`;
+    timelineTemplate.querySelector(".timeline_name").innerHTML = fatality.namesvg;
+    timelineTemplate.querySelector(".timeline_death_date").textContent =
+        fatality.death;
 
-        document.querySelector(".book").appendChild(timelineTemplate);
+    // Adding details to the infobox.
+    timelineTemplate.querySelector(
+        ".infobox_image"
+    ).src = `elements/${fatality.firstname}.svg`;
+    timelineTemplate.querySelector(
+        ".infobox_name"
+    ).innerHTML = fatality.namesvg;
+    timelineTemplate.querySelector(".infobox_birth_date").innerHTML +=
+        fatality.birth || "Unknown";
+    timelineTemplate.querySelector(".infobox_death_date").innerHTML +=
+        fatality.death;
+    timelineTemplate.querySelector(".infobox_story").innerHTML +=
+        fatality.story;
+    timelineTemplate.querySelector(".infobox_death_cause").innerHTML +=
+        fatality.causeofdeath;
 
-        let idAttribute;
+    // Adding the whole item to the HTML using the template.
+    document.querySelector(".book").appendChild(timelineTemplate);
 
+    let idAttribute;
+
+    // Adds hover function to devices that have a mouse pointer/is not touch.
+    if (!isTouchDeviceFunction()) {
         document
             .querySelector(".book")
             .lastElementChild.addEventListener(
@@ -97,6 +130,7 @@ function showTimeline(deathArray, getFatalityIndex) {
                     "block";
             }
         );
+
         document
             .querySelector(".book")
             .lastElementChild.addEventListener("mouseout", function hideInfobox() {
@@ -104,9 +138,15 @@ function showTimeline(deathArray, getFatalityIndex) {
                 "none";
         });
 
-        fatalityIndex++;
+    }
 
-        setTimeout(function() {
-            showTimeline(deathArray, fatalityIndex)
-        }, 200)
+    // Adds +1 to the fatalityIndex, so the next person in the array will be used next.
+    fatalityIndex++;
+
+    // Sets timeout for the next person to appear on the page after x seconds.
+    // We might be able to do this with an "animationend" listener instead, to be sure the first name is written
+    // before the next starts appearing?
+    setTimeout(function () {
+        showTimeline(deathArray, fatalityIndex)
+    }, 200)
 }
